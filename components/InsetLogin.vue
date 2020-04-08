c274d74387826af33e39c7a1b02c5be170f79768
songning <1457065857@qq.com>
2020-04-08 18:34:08 +0800

    inset-login

--- components/InsetLogin.vue
+++ components/InsetLogin.vue
@@ -1,13 +1,30 @@
 <template>
-
+    <view class="inset-login login-image full-size">
+        <button class="cu-btn block bg-red margin-tb-sm lg">手机登录</button>
+    </view>
 </template>
 
 <script>
+
     export default {
-        name: 'InsetLogin'
+        name: 'InsetLogin',
+        methods: {}
     };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .inset-login {
+        width: 100%;
+        height: 100%;
+
+        .cu-btn {
+            width: 400upx;
+            position: absolute;
+            top: 85%;
+            left: 50%;
+            z-index: 10;
+            transform: translate(-50%, -50%);
+        }
+    }
 
 </style>
