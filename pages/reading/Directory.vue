b3a4d01fce0be8081dd44e582cdca03c9e50a99b
songning <1457065857@qq.com>
2020-04-10 16:31:04 +0800

    目录 优化

--- pages/reading/Directory.vue
+++ pages/reading/Directory.vue
@@ -1,14 +1,5 @@
 <template>
     <view class="directory">
-        <scroll-view scroll-x class="nav">
-            <view class="flex text-center">
-                <view class="cu-item flex-sub" :class="{'text-red cur': item.key===tabCur}"
-                      v-for="(item,index) in sortTabs" :key="index" @tap="tabSelect" :data-sort="item.key">
-                    {{item.value}}
-                </view>
-            </view>
-        </scroll-view>
-        <view class="fill"></view>
         <view class="cu-list menu">
             <view class="cu-item arrow" :class="{'chosen-item': item.chaptersId === currentChapterId}"
                   v-for="(item, index) in directoryList" :key="index" hover-class='hover-class-style'
@@ -25,29 +16,32 @@
 <script>
     export default {
         name: 'Directory',
-        data () {
+        data() {
             return {
                 directoryList: [],
                 currentChapterId: '',
-                tabCur: 'positive',
-                halfHeight: 400,
-                sortTabs: [
-                    { key: 'positive', value: '正序' },
-                    { key: 'reverse', value: '倒序' }
-                ]
+                halfHeight: 400
             };
         },
-        onLoad (option) {
+        mounted() {
+            let doc = document.getElementsByClassName('uni-page-head-ft')[0];
+            doc.addEventListener('click', this.topBottomBtn);
+        },
+        destroyed() {
+            let doc = document.getElementsByClassName('uni-page-head-ft')[0];
+            doc.removeEventListener('click', this.topBottomBtn);
+        },
+        onLoad(option) {
             this.directoryList = JSON.parse(option.directory);
             this.currentChapterId = option.currentChapterId;
             if (this.directoryList && this.directoryList.length && this.currentChapterId) {
                 if (uni.getStorageSync('systemInfo') && uni.getStorageSync('systemInfo').windowHeight) {
-                    this.halfHeight = uni.getStorageSync('phoneInfo').windowHeight / 2;
+                    this.halfHeight = uni.getStorageSync('systemInfo').windowHeight / 2;
                 } else {
                     this.halfHeight = 400;
                     uni.getSystemInfo({
                         success: function (e) {
-                            uni.setStorageSync('phoneInfo', e);
+                            uni.setStorageSync('systemInfo', e);
                         }
                     });
                 }
@@ -55,32 +49,39 @@
             }
         },
         methods: {
-            jumpChapterBtn (chaptersId) {
+            jumpChapterBtn(chaptersId) {
                 if (this.currentChapterId !== chaptersId) {
                     let pages = getCurrentPages();
                     let prevPage = pages[pages.length - 2];  //上一个页面
                     prevPage.$vm.queryNewChapter(chaptersId, 'insert');
                 }
-                uni.navigateBack({
-                    delta: 1
+                uni.navigateBack({delta: 1});
+            },
+            scrollCenter() {
+                this.$nextTick(() => {
+                    uni.createSelectorQuery().select('.directory').boundingClientRect(data => {
+                        uni.createSelectorQuery().select('.chosen-item').boundingClientRect((res) => {
+                            uni.pageScrollTo({
+                                duration: 0,
+                                scrollTop: res.top - data.top - this.halfHeight
+                            });
+                        }).exec();
+                    }).exec();
                 });
             },
-            tabSelect (e) {
-                if (this.tabCur !== e.currentTarget.dataset.sort) {
-                    this.tabCur = e.currentTarget.dataset.sort;
-                    this.directoryList.reverse();
-                    this.scrollCenter();
+            topBottomBtn() {
+                let doc = document.getElementsByClassName('uni-page-head-ft')[0];
+                if (doc.innerHTML.includes('到底部')) {
+                    uni.pageScrollTo({scrollTop: 99999999999, duration: 300});
+                    setTimeout(() => {
+                        doc.innerHTML = doc.innerHTML.replace(/底/, '顶');
+                    }, 300);
+                } else if (doc.innerHTML.includes('到顶部')) {
+                    uni.pageScrollTo({scrollTop: 0, duration: 300});
+                    setTimeout(() => {
+                        doc.innerHTML = doc.innerHTML.replace(/顶/, '底');
+                    }, 300);
                 }
-            },
-            scrollCenter () {
-                uni.createSelectorQuery().select('.directory').boundingClientRect(data => {
-                    uni.createSelectorQuery().select('.chosen-item').boundingClientRect((res) => {
-                        uni.pageScrollTo({
-                            duration: 0,
-                            scrollTop: res.top - data.top - this.halfHeight
-                        });
-                    }).exec();
-                }).exec();
             }
         }
     };
@@ -88,17 +89,6 @@
 
 <style lang="scss" scoped>
     .directory {
-
-        .nav {
-            position: fixed;
-            background: #f7f6f2;
-            z-index: 2;
-        }
-
-        .fill {
-            height: 94upx;
-            width: 100%;
-        }
 
         .cu-item {
             background: unset;
