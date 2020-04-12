11266d8f5b6b176a62dae7cc1d31ab0f00392568
songning <1457065857@qq.com>
2020-04-12 18:18:28 +0800

    优化

--- pages/navigation/Bookcase.vue
+++ pages/navigation/Bookcase.vue
@@ -1,31 +1,25 @@
 <template>
-    <view class="book-case" :class="{'full-size': !userInfo}">
+    <view class="book-case">
         <view v-if="userInfo" class="cu-list full-size menu-avatar">
-            <view class="cu-item"
-                  v-for="(item,index) in result" :key="index"
-                  :class="[(modalName==='move-box-'+ index && sortType === '最近阅读') ? 'move-cur' : '', (modalName==='move-box-'+ index && sortType === '更新时间') ? 'move-no-top-cur':'']"
-                  @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd" @tap="startReading(item)"
-                  :data-target="'move-box-' + index">
-                <custom-image :url="item.coverUrl" class="image-size cu-avatar radius xl"></custom-image>
-                <view class="content">
-                    <view class="text-black text-df">{{item.title || '暂无'}}</view>
-                    <view class="text-grey text-sm">{{item.author || '暂无'}}</view>
-                    <view class="text-gray text-xs text-cut">
-                        <text class="margin-right-xs">{{convertDate(item.updateTime)}} ·</text>
-                        <text>{{item.chapter || '暂无'}}</text>
+            <scroll-view scroll-y="true" style="height: 100%;">
+                <view class="cu-item" v-for="(item,index) in result" :key="index" @tap="startReading(item)">
+                    <custom-image :url="item.coverUrl" class="image-size cu-avatar radius xl"></custom-image>
+                    <view class="content">
+                        <view class="text-black text-df">{{item.title || '未知书名'}}</view>
+                        <view class="text-grey text-sm">{{item.author || '未知作者'}}</view>
+                        <view class="text-gray text-xs text-cut">
+                            <text class="margin-right-xs">{{convertDate(item.updateTime)}} ·</text>
+                            <text>{{item.chapter || '未知章节'}}</text>
+                        </view>
                     </view>
                 </view>
-                <view class="move" :class="{'move-no-top':sortType === '更新时间'}">
-                    <view class="bg-grey" v-if="sortType === '最近阅读'" @tap.stop="topBtn(item.novelsId)">置顶</view>
-                    <view class="bg-red" @tap.stop="deleteBtn(item.novelsId, index)">删除</view>
+                <view class="cu-item">
+                    <view class="cu-avatar radius lg add-image"></view>
+                    <view class="content">
+                        <view class="text-black text-bold" @tap="navChange">收藏小说</view>
+                    </view>
                 </view>
-            </view>
-            <view class="cu-item">
-                <view class="cu-avatar radius lg add-image"></view>
-                <view class="content">
-                    <view class="text-black text-bold" @tap="navChange">收藏小说</view>
-                </view>
-            </view>
+            </scroll-view>
         </view>
         <inset-login v-else></inset-login>
     </view>
@@ -43,10 +37,6 @@
         components: { InsetLogin },
         data () {
             return {
-                modalName: '',
-                listTouchStart: 0,
-                listTouchDirection: null,
-                // 假数据
                 result: [],
                 toDeleteInfo: {}
             };
@@ -90,23 +80,6 @@
             uni.navigateTo({url: '/pages/navigation/Search'});
         },
         methods: {
-            // ListTouch触摸开始
-            touchStart (e) {
-                this.listTouchStart = e.touches[0].pageX;
-            },
-            // ListTouch计算方向
-            touchMove (e) {
-                this.listTouchDirection = e.touches[0].pageX - this.listTouchStart > 0 ? 'right' : 'left';
-            },
-            // ListTouch计算滚动
-            touchEnd (e) {
-                if (this.listTouchDirection === 'left') {
-                    this.modalName = e.currentTarget.dataset.target;
-                } else {
-                    this.modalName = null;
-                }
-                this.listTouchDirection = null;
-            },
             startReading (item) {
                 uni.navigateTo({ url: '/pages/reading/SimpleRead?novels=' + encodeURIComponent(JSON.stringify(item)) });
             },
@@ -191,6 +164,7 @@
 <style lang="scss" scoped>
     .book-case {
         height: 100%;
+        width: 100%;
         .cu-list {
             overflow-y: auto;
 
@@ -198,29 +172,29 @@
                 justify-content: unset;
                 padding-right: unset;
                 background: unset;
+                position: relative;
+                display: flex;
+                height: 140upx;
+                -webkit-box-align: center;
+                -webkit-align-items: center;
+                align-items: center;
 
                 .image-size {
-                    width: 105rpx;
+                    width: 105upx;
                 }
 
                 .cu-avatar {
                     float: left;
-                    left: 15rpx;
+                    left: 15upx;
                 }
 
                 .content {
                     float: left;
-                    left: 135rpx;
-                    width: calc(100% - 140rpx - 20rpx);
+                    left: 135upx;
+                    width: calc(100% - 140upx - 20upx);
+                    position: absolute;
+                    line-height: 1.6em;
                 }
-
-                .move-no-top {
-                    width: 140rpx;
-                }
-            }
-
-            .move-no-top-cur {
-                transform: translateX(-140rpx);
             }
         }
     }
