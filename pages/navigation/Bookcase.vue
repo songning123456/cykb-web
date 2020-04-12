58f63c3bb1aac9494d9ae9ce943f5ee31999b6f0
songning <1457065857@qq.com>
2020-04-12 22:55:53 +0800

    书架优化

--- pages/navigation/Bookcase.vue
+++ pages/navigation/Bookcase.vue
@@ -12,14 +12,22 @@
                             <text>{{item.chapter || '未知章节'}}</text>
                         </view>
                     </view>
+                    <view class="action delete-icon" v-if="isToDelete">
+                        <radio class="round" :class="{'checked': deleteList.includes(item.novelsId)}" :checked="deleteList.includes(item.novelsId)"
+                               :value="item.novelsId" @tap.stop="changeDelBtn(item.novelsId)"></radio>
+                    </view>
                 </view>
-                <view class="cu-item">
+                <view class="cu-item" v-if="!isToDelete">
                     <view class="cu-avatar radius lg add-image"></view>
                     <view class="content">
                         <view class="text-black text-bold" @tap="navChange">收藏小说</view>
                     </view>
                 </view>
             </scroll-view>
+            <view class="delete-bottom text-lg" :style="{bottom:isToDelete ? 0 : '-110upx'}">
+                <view class="left" @tap.stop="DelSetBtn('all')">{{deleteList.length !== result.length ? '全选' : '取消全选'}}</view>
+                <view class="right text-red" @tap.stop="DelSetBtn('delete')">删除{{deleteList.length ? '(' + deleteList.length + ')' : ''}}</view>
+            </view>
         </view>
         <inset-login v-else></inset-login>
     </view>
@@ -38,7 +46,9 @@
         data () {
             return {
                 result: [],
-                toDeleteInfo: {}
+                toDeleteInfo: {},
+                deleteList: [],
+                isToDelete: false
             };
         },
         computed: {
@@ -67,30 +77,49 @@
             },
             sortType (newVal, oldVal) {
                 this.queryBookcase();
+            },
+            isToDelete(newVal, oldVal) {
+                let ftDoc = document.getElementsByClassName('uni-page-head-ft') && document.getElementsByClassName('uni-page-head-ft')[0];
+                if (newVal) {
+                    ftDoc.style.display = 'none';
+                } else {
+                    ftDoc.style.display = 'inherit';
+                }
             }
         },
         onPullDownRefresh () {
-            if (this.userInfo) {
+            if (this.userInfo && !this.isToDelete) {
                 this.queryBookcase();
             } else {
                 uni.stopPullDownRefresh();//得到数据后停止下拉刷新
             }
         },
         onNavigationBarButtonTap(e) {
-            if (e.type === 'delete') {
-                console.error(e);
-            } else {
-                uni.navigateTo({url: '/pages/navigation/Search'});
+            if (e.type === 'delete' && this.result.length) {
+                if (this.isToDelete) {
+                    this.isToDelete = false;
+                    this.deleteList = [];
+                } else {
+                    this.isToDelete = true;
+                }
+            }else {
+                if (!this.isToDelete) {
+                    uni.navigateTo({url: '/pages/navigation/Search'});
+                }
             }
         },
         methods: {
             startReading (item) {
-                uni.navigateTo({ url: '/pages/reading/SimpleRead?novels=' + encodeURIComponent(JSON.stringify(item)) });
+                if (!this.isToDelete) {
+                    uni.navigateTo({ url: '/pages/reading/SimpleRead?novels=' + encodeURIComponent(JSON.stringify(item)) });
+                }
             },
             navChange () {
-                uni.switchTab({
-                    url: '/pages/navigation/Classify',
-                });
+                if (!this.isToDelete) {
+                    uni.switchTab({
+                        url: '/pages/navigation/Classify',
+                    });
+                }
             },
             convertDate (updateTime) {
                 return convertDate.convertZh(currentDate, updateTime);
@@ -113,53 +142,57 @@
                     uni.stopPullDownRefresh();//得到数据后停止下拉刷新
                 });
             },
-            topBtn (novelsId) {
-                let params = {
-                    condition: {
-                        uniqueId: this.userInfo.uniqueId,
-                        novelsId: novelsId
-                    }
-                };
-                uni.showLoading({ title: 'loading...', mask: true });
-                request.post('/relation/topBookcase', params).then(data => {
-                    if (data.status === 200) {
-                        let temp = [];
-                        this.result.forEach(item => {
-                            if (item.novelsId === novelsId) {
-                                temp.unshift(item);
-                            } else {
-                                temp.push(item);
-                            }
-                        });
-                        this.result = temp;
-                    }
-                }).finally(() => {
-                    uni.hideLoading();
-                });
+            changeDelBtn(novelsId) {
+               if (this.deleteList.includes(novelsId)) {
+                   let index = this.deleteList.indexOf(novelsId);
+                   this.deleteList.splice(index, 1);
+               } else {
+                   this.deleteList.push(novelsId);
+               }
             },
-            deleteBtn (novelsId, index) {
-                uni.showModal({
-                    title: '提示',
-                    content: '确定从书架中移除此本小说?',
-                    success: (res) => {
-                        if (res.confirm) {
-                            let params = {
-                                condition: {
-                                    uniqueId: this.userInfo.uniqueId,
-                                    novelsId: novelsId
-                                }
-                            };
-                            uni.showLoading({ title: 'loading...', mask: true });
-                            request.post('/relation/deleteBookcase', params).then(data => {
-                                if (data.status === 200) {
-                                    this.result.splice(index, 1);
-                                }
-                            }).finally(() => {
-                                uni.hideLoading();
-                            });
-                        }
+            DelSetBtn(type) {
+                if (type === 'all') {
+                    if (this.deleteList.length !== this.result.length) {
+                        this.result.forEach(item => {
+                            if (!this.deleteList.includes(item.novelsId)) {
+                                this.deleteList.push(item.novelsId);
+                            }
+                        })
+                    } else {
+                        this.deleteList = [];
                     }
-                });
+                } else {
+                    if (!this.deleteList.length) {
+                        return;
+                    }
+                    uni.showModal({
+                        title: '提示',
+                        content: '确定从书架中移除选中小说?',
+                        success: (res) => {
+                            if (res.confirm) {
+                                let params = {
+                                    condition: {
+                                        uniqueId: this.userInfo.uniqueId,
+                                        novelsIdList: this.deleteList
+                                    }
+                                };
+                                uni.showLoading({ title: 'loading...', mask: true });
+                                let tempList = this.deleteList;
+                                request.post('/relation/deleteBookcase', params).then(data => {
+                                    if (data.status === 200) {
+                                        this.result = this.result.filter(item => !tempList.includes(item.novelsId));
+                                    }
+                                }).finally(() => {
+                                    uni.hideLoading();
+                                });
+                            }
+                        },
+                        complete: () => {
+                            this.isToDelete = false;
+                            this.deleteList = [];
+                        }
+                    });
+                }
             }
         }
     };
@@ -198,6 +231,49 @@
                     width: calc(100% - 140upx - 20upx);
                     position: absolute;
                     line-height: 1.6em;
+                }
+
+                .delete-icon {
+                    position: absolute;
+                    right: 2%;
+
+                    /deep/ uni-radio::before, uni-checkbox::before {
+                        color: #f7f6f2 !important;
+                    }
+
+                    /deep/ uni-radio .uni-radio-input {
+                        background-color: unset;
+                    }
+
+                    /deep/ uni-radio:not([disabled]) .uni-radio-input:hover {
+                         border-color: #d1d1d1;
+                    }
+                }
+            }
+            .delete-bottom {
+                position: fixed;
+                height: 100upx;
+                transition: all 0.2s;
+                width: 100%;
+                z-index: 1000;
+                background-color: rgb(247, 246, 242);
+
+                .left, .right {
+                    height: 100%;
+                    float: left;
+                    text-align: center;
+                    line-height: 100upx;
+                    letter-spacing: 1upx;
+                    border-top: 1upx solid rgba(0, 0, 0, 0.33);
+                }
+
+                .left {
+                    width: 50%;
+                    border-right: 1upx solid rgba(0, 0, 0, 0.33);
+                }
+
+                .right {
+                    width: calc(50% - 1upx);
                 }
             }
         }
